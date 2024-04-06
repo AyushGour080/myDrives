4abb1850b4d085daab4da516cd5156e1a5fee18f
Ayushh <ayushgour080@gmail.com>
2024-04-06 10:46:57 +0530

    sidebar and datafile created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Random } from "./components/ramdom";
+import React, { useState } from "react";
+import { NavBar } from "./components/NavBar";
+import { FileData } from "./components/FileData";
+import { SideBar } from "./components/SideBar";
 
 function App() {
-  return <Random />;
+  const [files, setFiles] = useState([
+    { f1: "abc", f2: "efg" },
+    { f1: "abc", f2: "efg" },
+    { f1: "abc", f2: "efg" },
+    { f1: "abc", f2: "efg" },
+  ]);
+  return (
+    <>
+      <NavBar />
+      <div className="data">
+        <SideBar files={files} />
+
+        <FileData files={files} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/FileData.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./FileData.css";
+import { popUp } from "./popUp";
+export const FileData = (props) => {
+  const data = props.files.map((f) => (
+    <div className="out">
+      <img
+        className=" fileImage h-21 w-20 "
+        src="https://th.bing.com/th/id/OIP.yl502gIVfOAR4EzgTGkFbgAAAA?pid=ImgDet&w=176&h=238&c=7&dpr=1.7"
+      />
+      <p>filename -{f.f1}</p>
+    </div>
+  ));
+  return (
+    <>
+      <div className="FileData">
+        <div className="grid-col">{data}</div>
+        <div className="grid-col">{data}</div>
+        <div className="grid-col">{data}</div>
+        <div className="grid-col">{data}</div>
+      </div>
+
+      <popUp />
+    </>
+  );
+};
